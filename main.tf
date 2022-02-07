48e74596b5b3600928a4b42c6e3a762dad08bb37
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-07 22:53:37 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,7 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
 
-  subscription_id   = "7ed30da0-d878-475f-85b0-f1f907a3a03b"
-  tenant_id         = "7ed30da0-d878-475f-85b0-f1f907a3a03b"
-  client_id         = "a9a20e58-d742-44a5-9846-4f2fcdb8213b"
-  client_secret     = "5xUWcFhXVTnz.afL4ib6EyyJpX1v.Im7rC"
-}
 resource "azurerm_resource_group" "rg" {
   #name     = "myTFResourceGroup"
   name     = var.resource_group_name
