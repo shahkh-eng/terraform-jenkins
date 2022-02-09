0f55095c33fe6f78905d5e3957b8917a16c7c020
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-09 23:24:03 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ terraform {
  #     version = "~> 2.65"
     }
   }
+  backend "remote" {
+    resource_group_name  = "jonnychipz-state"
+    storage_account_name = "jonnychipztfstate"
+    container_name       = "tstate"
+    key                  = "terraform.tfstate"
+  }
 #  required_version = ">= 1.1.0"
 }
 
@@ -19,12 +25,7 @@ provider "azurerm" {
   client_secret     = "5xUWcFhXVTnz.afL4ib6EyyJpX1v.Im7rC"
 }
 
-backend "azurerm" {
-  resource_group_name  = "jonnychipz-state"
-  storage_account_name = "jonnychipztfstate"
-  container_name       = "tstate"
-  key                  = "terraform.tfstate"
-}
+
 
 resource "azurerm_resource_group" "rg" {
   #name     = "myTFResourceGroup"
