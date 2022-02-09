9a94cd4bdc22924bb6c5e503da04c492f9813cb7
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-09 22:02:01 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+ #     version = "~> 2.65"
     }
   }
-  required_version = ">= 1.1.0"
+#  required_version = ">= 1.1.0"
   backend "azurerm" {
     resource_group_name  = "jonnychipz-state"
     storage_account_name = "jonnychipztfstate"
@@ -15,7 +15,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
 
+  subscription_id   = "7ed30da0-d878-475f-85b0-f1f907a3a03b"
+  tenant_id         = "7ed30da0-d878-475f-85b0-f1f907a3a03b"
+  client_id         = "a9a20e58-d742-44a5-9846-4f2fcdb8213b"
+  client_secret     = "5xUWcFhXVTnz.afL4ib6EyyJpX1v.Im7rC"
+}
 
 resource "azurerm_resource_group" "rg" {
   #name     = "myTFResourceGroup"
