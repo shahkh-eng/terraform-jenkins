272a33332058683e58f0b323a95883ae8a34d5c4
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-10 13:07:22 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
   tenant_id         = "7ed30da0-d878-475f-85b0-f1f907a3a03b"
   client_id         = "a9a20e58-d742-44a5-9846-4f2fcdb8213b"
   client_secret     = "5xUWcFhXVTnz.afL4ib6EyyJpX1v.Im7rC"
-  version         = "=2.0.0"
+
   features {}
 }
