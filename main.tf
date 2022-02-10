a89d1940763cebbfd436ec041681b4613b18f7e2
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-10 12:46:50 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
- #     version = "~> 2.65"
+  #    version = "~>2.0"
+      version = "~> 2.65"
     }
   }
   backend "azurerm" {
