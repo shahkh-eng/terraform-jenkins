f9776afe9d2c53be0e2f5a43ab1513141b4d5f44
Khurram Javed <54314239+shahkh-eng@users.noreply.github.com>
2022-02-10 14:51:37 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,10 +22,3 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-# Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "centralindia"
-  resource_group_name = azurerm_resource_group.rg.name
-}
